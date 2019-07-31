228b1c78f4dd818fd1b96b81e10362e1acc3bd04
Fhaidy Maciel <fhaidy@outlook.com>
2019-07-31 12:48:47 -0300

    Arquivos das aulas sobre File, Directory e Path

---
+++ Directory Aula/Directory Aula/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Directory_Aula {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:\temp\myFolder";
+            try {
+
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Folders:");
+                foreach (string folder in folders) {
+                    Console.WriteLine(folder);
+                }
+
+                Console.WriteLine();
+
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Files:");
+                foreach (string file in files) {
+                    Console.WriteLine(file);
+                }
+
+                Console.WriteLine();
+
+                Directory.CreateDirectory(path + @"\newFolder");
+
+            } catch (IOException e) {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ File Aula/File Aula/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace File_Aula {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:\temp\file1.txt";
+            string path2 = @"c:\temp\file2.txt";
+
+            try {
+
+                string[] lines = File.ReadAllLines(path);
+
+                using (StreamWriter sw = File.AppendText(path2)) {
+                    foreach (string line in lines) {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+
+            } catch (IOException e) {
+
+                Console.WriteLine("An error occurred:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
