9ba6134897c602e4ae3c8b270ea7a0410b4c2657
Fhaidy Maciel <fhaidy@outlook.com>
2019-08-24 07:00:11 -0300

    Adicionado Novas entities e uma nova migration

--- SalesWebMvc2/SalesWebMvc2/Models/Department.cs
+++ SalesWebMvc2/SalesWebMvc2/Models/Department.cs
@@ -8,6 +8,7 @@ namespace SalesWebMvc2.Models {
 
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
         public Department() {
         }
@@ -16,5 +17,15 @@ namespace SalesWebMvc2.Models {
             Id = id;
             Name = name;
         }
+
+        public void AddSeller (Seller seller) {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final) {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+
+        
     }
 }
