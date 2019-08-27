a22bed9541e3e01a501f767f1c443e5803ef20c3
Fhaidy Maciel <fhaidy@outlook.com>
2019-08-27 18:12:38 -0300

    Criado formulario de cadastro de Seller

--- SalesWebMvc2/SalesWebMvc2/Controllers/SellersController.cs
+++ SalesWebMvc2/SalesWebMvc2/Controllers/SellersController.cs
@@ -3,21 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc2.Models;
 using SalesWebMvc2.Services;
 
 namespace SalesWebMvc2.Controllers
 {
-    public class SellersController : Controller
-    {
+    public class SellersController : Controller {
         private readonly SellerService _sellerService;
 
         public SellersController(SellerService sellerService) {
             _sellerService = sellerService;
         }
-        public IActionResult Index()
-        {
+        public IActionResult Index() {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- SalesWebMvc2/SalesWebMvc2/Services/SellerService.cs
+++ SalesWebMvc2/SalesWebMvc2/Services/SellerService.cs
@@ -14,6 +14,11 @@ namespace SalesWebMvc2.Services {
         public List<Seller> FindAll() {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj) {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 
     
